64846485421d7b5dee1e95723fa1738864622bc2
GuilhermeAlamino <guilhermedevelopedweb@hotmail.com>
2023-08-05 21:36:08 -0300

    desenvolvido as rotas

---
+++ src/Routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import DashboardHome from './pages/dashboard/content/home.js';
+import Employee from './pages/dashboard/employee';
+import EmployeeStore from './pages/dashboard/employee/store';
+import EmployeeShow from './pages/dashboard/employee/show';
+import EmployeeDelete from './pages/dashboard/employee/delete';
+import Department from './pages/dashboard/department';
+import DepartmentStore from './pages/dashboard/department/store';
+import DepartmentShow from './pages/dashboard/department/show';
+import DepartmentDelete from './pages/dashboard/department/delete';
+import Task from './pages/dashboard/task';
+import TaskStore from './pages/dashboard/task/store';
+import TaskShow from './pages/dashboard/task/show';
+import TaskDelete from './pages/dashboard/task/delete';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="/dashboard/home" element={<DashboardHome />}></Route>
+          <Route path="/dashboard/employee" element={<Employee />}></Route>
+          <Route path="/dashboard/employee/store" element={<EmployeeStore />}></Route>
+          <Route path="/dashboard/employee/show/:id" element={<EmployeeShow />}></Route>
+          <Route path="/dashboard/employee/delete/:id" element={<EmployeeDelete />}></Route>
+          <Route path="/dashboard/department" element={<Department />}></Route>
+          <Route path="/dashboard/department/store" element={<DepartmentStore />}></Route>
+          <Route path="/dashboard/department/show/:id" element={<DepartmentShow />}></Route>
+          <Route path="/dashboard/department/delete/:id" element={<DepartmentDelete />}></Route>
+          <Route path="/dashboard/task" element={<Task />}></Route>
+          <Route path="/dashboard/task/store" element={<TaskStore />}></Route>
+          <Route path="/dashboard/task/show/:id" element={<TaskShow />}></Route>
+          <Route path="/dashboard/task/delete/:id" element={<TaskDelete />}></Route>
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
